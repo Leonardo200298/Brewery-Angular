f1310d1c2af946251309e6be34d09a0462ffa389
Leonardo200298 <leonardosanchez119@gmail.com>
2022-09-30 13:06:43 -0300

    Feat:add three new objetcs to beer-list.component and make tables on beer-list.component.html, add new content to github

--- src/app/beer-list/beer-list.component.ts
+++ src/app/beer-list/beer-list.component.ts
@@ -6,7 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./beer-list.component.scss']
 })
 export class BeerListComponent implements OnInit {
-
+  beer = {
+    "nombre":"La negra",
+    "Tipo":"Porter",
+    "Precio":"$400",
+    "IBU":"%24.5",
+    "Stock":"107"
+}
+beer2 = {
+  "nombre":"La Miel",
+  "Tipo":"Honey",
+  "Precio":"$420",
+  "IBU":"%12.3",
+  "Stock":"140"
+}
+beer3 = {
+  "nombre":"la ger",
+  "Tipo":"lager",
+  "Precio":"$390",
+  "IBU":"%11.3",
+  "Stock":"138"
+}
   constructor() { }
 
   ngOnInit(): void {
