263490f3ffce01e871ea5bac28ccb26b1aec1071
Leonardo200298 <leonardosanchez119@gmail.com>
2022-10-03 12:59:56 -0300

    Feat:Modified objects for object array and render

--- src/app/beer-list/beer-list.component.ts
+++ src/app/beer-list/beer-list.component.ts
@@ -8,27 +8,27 @@ import { Beer } from './beer';
 
 export class BeerListComponent implements OnInit {
   
-  beer:Beer = {
+  beer:Beer[] = [{
     nombre: "La negra",
     tipo: "Porter",
     precio: 400,
     ibu: 24.7,
     stock: 107,
-  }
-  beer2:Beer = {
+  },
+  {
     nombre: "La Miel",
     tipo: "Honey",
     precio: 420,
     ibu: 12.3,
     stock: 140
-  }
-  beer3:Beer = {
+  },
+  {
     nombre: "la ger",
     tipo: "lager",
     precio: 390,
     ibu: 11.3,
     stock: 138
-  }
+  }]
 
   constructor() { }
 
