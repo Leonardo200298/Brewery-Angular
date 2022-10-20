104949debac29f4c1e0bbb39f6388fa6fe0c23bf
Leonardo200298 <leonardosanchez119@gmail.com>
2022-10-20 11:59:20 -0300

    Fead:add new component about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BeerListComponent } from './beer-list/beer-list.component';
+import { BreweryAboutComponent } from './brewery-about/brewery-about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BeerListComponent
+    BeerListComponent,
+    BreweryAboutComponent
   ],
   imports: [
     BrowserModule,
